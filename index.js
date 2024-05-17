d22124b5ae0888ac1a36fabcdc8740472f6b195f
chris <chrisrolling27@gmail.com>
2024-05-17 14:58:32 -0700

    views

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const { engine } = require('express-handlebars');
+const path = require('path');
 const PORT = 3000;
-
-app.use(express.static('public'));
-app.use(express.json());
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/clicked', (req, res) => {
-    const spaceName = req.query.spaceName;
-    console.log('Space clicked:', spaceName);
-    res.json({ success: true, message: `Space ${spaceName} clicked` });
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+  });
+app.use(express.static("public"));
+app.use(express.json());
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.post("/clicked", (req, res) => {
+  const { spaceName } = req.body;
+  console.log("Space clicked:", spaceName);
+  res.json({ success: true, message: `Space ${spaceName} clicked` });
+});
+
+
+app.get("/space/:spaceId", (req, res) => {
+    const { spaceId } = req.params; 
+  
+    // Example data - you might replace this with actual data fetching logic later
+    const data = {
+      spaceId: spaceId,
+      content: "This is your new space. Start adding content here!"
+    };
+  
+    // Render a Handlebars template (assuming you have a 'space.handlebars' template)
+    res.render("space", data);
+  });
